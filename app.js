6654b7361b31178552cecde15d83e4161d064801
panosjapan7 <panosjapan@gmail.com>
2023-05-15 14:06:19 +0200

    Creates local server, Connects to MongoDB, "/api" route

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/user-routes");
+const app = express();
+
+app.use(express.json());
+app.use("/api", router);
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:FQj03JOIgSi6we2s@cluster0.nrlaqu0.mongodb.net/auth?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log("Database is connected! Listening to localhost 5000");
+  })
+  .catch((err) => console.log(err));
+
+// FQj03JOIgSi6we2s
