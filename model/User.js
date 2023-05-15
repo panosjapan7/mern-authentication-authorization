80f2efce0ff9ebe881ec91fb8f8a9a2a82aa0a87
panosjapan7 <panosjapan@gmail.com>
2023-05-15 14:06:33 +0200

    Creates User Schema

---
+++ model/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: true,
+    minLength: 6,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
