e5feb64b083babf743074de1d8c696be6652aa5b
panosjapan7 <panosjapan@gmail.com>
2023-05-15 14:58:40 +0200

    Creates "/user" route and uses middleware to verify token & show user data

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { signup, login } = require("../controllers/user-controller");
+const {
+  signup,
+  login,
+  verifyToken,
+  getUser,
+} = require("../controllers/user-controller");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user", verifyToken, getUser);
 
 module.exports = router;
