e91836d452563638839d1591bdaed3ca06a9c0fb
panosjapan7 <panosjapan@gmail.com>
2023-05-16 09:04:11 +0200

    Creates Login functionality, Redirects to "/user"

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const Login = () => {
-  return <div>Login</div>;
+  const history = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const sendRequest = async () => {
+    const response = await axios
+      .post("http://localhost:5000/api/login", {
+        email: inputs.email,
+        password: inputs.password,
+      })
+      .catch((err) => console.log(err));
+    const data = await response.data;
+    return data;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendRequest().then(() => history("/user"));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          marginLeft="auto"
+          marginRight="auto"
+          width={300}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h2">Login</Typography>
+          <TextField
+            name="email"
+            onChange={handleChange}
+            type="email"
+            value={inputs.email}
+            variant="outlined"
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            type="password"
+            value={inputs.password}
+            variant="outlined"
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
