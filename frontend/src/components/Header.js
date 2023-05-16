cffb4a8f895fbc76e145d8cb657fe40cbeafc79c
panosjapan7 <panosjapan@gmail.com>
2023-05-16 08:18:24 +0200

    Creates and styles Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [value, setValue] = useState();
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h3">MernAuth</Typography>
+          <Box sx={{ marginLeft: "auto" }}>
+            <Tabs
+              indicatorColor="secondary"
+              onChange={(e, val) => setValue(val)}
+              value={value}
+              textColor="inherit"
+            >
+              <Tab label="Login" />
+              <Tab label="Signup" />
+            </Tabs>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Header;
