a6ed11fcc8dc887cd6d5cf740258fbb5c536e2f6
panosjapan7 <panosjapan@gmail.com>
2023-05-16 08:44:37 +0200

    Creates frontend of form

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,7 +1,70 @@
-import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          marginLeft="auto"
+          marginRight="auto"
+          width={300}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h2">Signup</Typography>
+          <TextField
+            name="name"
+            onChange={handleChange}
+            value={inputs.name}
+            variant="outlined"
+            placeholder="Name"
+            margin="normal"
+          />
+          <TextField
+            name="email"
+            onChange={handleChange}
+            type="email"
+            value={inputs.email}
+            variant="outlined"
+            placeholder="Email"
+            margin="normal"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            type="password"
+            value={inputs.password}
+            variant="outlined"
+            placeholder="Password"
+            margin="normal"
+          />
+          <Button variant="contained" type="submit">
+            Signup
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
