28196f6bffa09b92af75d264ac9f5942aa1896f6
panosjapan7 <panosjapan@gmail.com>
2023-05-16 08:28:18 +0200

    Adds Routes to /login, /signup, /user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user" element={<Welcome />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
