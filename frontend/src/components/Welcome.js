090be287ca923c9084d1b9df18199b4f4f019228
panosjapan7 <panosjapan@gmail.com>
2023-05-17 07:52:26 +0200

    Checks if user is logged in

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const [user, setUser] = useState();
+
+  const sendRequest = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/user", {
+        withCredentials: true,
+      });
+      console.log("res:", res);
+      const data = res.data;
+      return data;
+    } catch (err) {
+      console.log(err);
+      throw err; // Throw the error to reject the promise
+    }
+  };
+
+  useEffect(() => {
+    sendRequest()
+      .then((data) => setUser(data.user))
+      .catch((error) => console.log("Error: ", error));
+  }, []);
+
+  return (
+    <div>
+      <p>HELLO</p>
+      {user && <h1>{user.name}</h1>}
+    </div>
+  );
 };
 
 export default Welcome;
